9bff161720da150a6b7da6f27f7d4ed7e27779f0
renea <reneadonayfrancoperez@gmail.com>
2023-12-03 13:11:07 -0500

    update projects with bot feature and icon

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
     if(this.scrollY > 20){
       $(".navbar").addClass("sticky");
       $(".goTop").fadeIn();
+
     }
     else{
       $(".navbar").removeClass("sticky");
       $(".goTop").fadeOut();
+
     }
   });
 
